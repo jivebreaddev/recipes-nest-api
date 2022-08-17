3f60c67a08a3bd214e8e466f5424466b305c6303
Jivebreaddev <jivebreaddev@gmail.com>
2022-08-17 11:14:21 +0900

    Add User entity and stub for testing

--- recipes/src/user/entities/user.entity.ts
+++ recipes/src/user/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
