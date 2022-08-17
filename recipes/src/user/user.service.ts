85ad6b2ce9e83b9dae52c50e52f6e3f3d9ab9391
Jivebreaddev <jivebreaddev@gmail.com>
2022-08-17 13:26:18 +0900

    Add User Service repository functions

--- recipes/src/user/user.service.ts
+++ recipes/src/user/user.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.usersRepository.create({
+      username: createUserDto.username,
+      password: createUserDto.password,
+    });
+    return user;
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll(): Promise<User[]> {
+    return await this.usersRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number): Promise<User> {
+    return await this.usersRepository.findOneBy({ id: id });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('recipe not found2');
+    }
+    Object.assign(user, updateUserDto);
+    return await user;
   }
 }
