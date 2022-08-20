500ea2431f805ab9f7da1fb86bc9fe263784aa8c
Jivebreaddev <jivebreaddev@gmail.com>
2022-08-20 14:12:31 +0900

    Add ingredient Service to Recipe Module

--- recipe-api/src/recipe/recipe.module.ts
+++ recipe-api/src/recipe/recipe.module.ts
@@ -3,9 +3,10 @@ import { RecipeService } from './recipe.service';
 import { RecipeController } from './recipe.controller';
 import { Recipe } from './entities/recipe.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { IngredientService } from './ingredient/ingredient.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Recipe])],
   controllers: [RecipeController],
-  providers: [RecipeService],
+  providers: [RecipeService, IngredientService],
 })
 export class RecipeModule {}
