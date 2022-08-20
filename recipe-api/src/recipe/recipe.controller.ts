1c7bac3fceda7f0971c98a1044c901f14c45a819
Jivebreaddev <jivebreaddev@gmail.com>
2022-08-20 19:47:57 +0900

    Add CRUD for Ingredient

--- recipe-api/src/recipe/recipe.controller.ts
+++ recipe-api/src/recipe/recipe.controller.ts
@@ -10,10 +10,16 @@ import {
 import { RecipeService } from './recipe.service';
 import { CreateRecipeDto } from './dto/create-recipe.dto';
 import { UpdateRecipeDto } from './dto/update-recipe.dto';
+import { IngredientService } from './ingredient/ingredient.service';
+import { UpdateIngredientDto } from './dto/update-ingredient.dto';
+import { CreateIngredientDto } from './dto/create-ingredient.dto';
 
 @Controller('recipe')
 export class RecipeController {
-  constructor(private readonly recipeService: RecipeService) {}
+  constructor(
+    private readonly recipeService: RecipeService,
+    private ingredientService: IngredientService,
+  ) {}
   @Post()
   create(@Body() createRecipeDto: CreateRecipeDto) {
     return this.recipeService.create(createRecipeDto);
@@ -33,5 +39,29 @@ export class RecipeController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.recipeService.remove(+id);
+  }
+
+  @Post('/ingredient')
+  create_ingredient(@Body() createIngredientDto: CreateIngredientDto) {
+    return this.ingredientService.create(createIngredientDto);
+  }
+  @Get('/ingredient')
+  findAll_ingredient() {
+    return this.ingredientService.findAll();
+  }
+  @Get('/ingredient/:id')
+  findOne_ingredient(@Param('id') id: string) {
+    return this.ingredientService.findOne(+id);
+  }
+  @Patch('/ingredient/:id')
+  update_ingredient(
+    @Param('id') id: string,
+    @Body() updateIngredientDto: UpdateIngredientDto,
+  ) {
+    return this.ingredientService.update(+id, updateIngredientDto);
+  }
+  @Delete('/ingredient/:id')
+  remove_ingredient(@Param('id') id: string) {
+    return this.ingredientService.remove(+id);
   }
 }
